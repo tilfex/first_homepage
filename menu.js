ed219ba90cd651eba0271ba85b13e0923c92d530
tilfex <felix.trinler@mail.de>
2021-07-11 11:20:38 +0200

    initialization

---
+++ menu.js
@@ -0,0 +1,23 @@
+let a = [];
+
+$.get("english_all.json", function(data, status){
+    a = data;
+    console.log(a)
+})
+
+class ShoppingList extends React.Component {
+    render() {
+      return (
+        <div className="shopping-list">
+          <h1>Shopping List for {this.props.name}</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      );
+    }
+  }
+
+ReactDOM.render(<ShoppingList />, document.getElementById('menu'));
